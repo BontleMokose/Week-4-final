3d1b940fccaf3734fca731780dcd91355f23ee1b
BontleMokose <mokosebontle@gmail.com>
2023-03-19 20:02:44 -0700

    Week 5 final

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import WeatherSearch from "./WeatherSearch";
+
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <div className="App">
-      <h1>Weather App</h1>
-      <WeatherSearch />
-    </div>
+    <App />
   </StrictMode>
 );
